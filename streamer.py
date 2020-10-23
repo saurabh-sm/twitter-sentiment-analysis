ed4edc8909bb5a77a9f53ca2a987ef27e958c3e5
Saurabh Mishra <saurab.mish@gmail.com>
2020-10-23 09:56:32 -0700

    connect to Twitter API to stream tweets based on hash tags

--- streamer.py
+++ streamer.py
@@ -1,32 +1,29 @@
-import os
-
-from dotenv import load_dotenv, find_dotenv
 from tweepy import OAuthHandler
 from tweepy import Stream
-from tweepy.streaming import StreamListener
 
-class TweetListener(StreamListener):
-    '''
-    Stream filtered tweets to the standard output
-    '''
-    def on_data(self, data):
-        print(data)
-        return True
+import keys
+from listener import Listener
 
-    def on_error(self, status):
-        print(status)
+class Streamer():
+    '''
+    Stream tweets based on hash tags.
+    '''
+    def stream_tweets(self, tweets_file, hash_tags):
+        listener = Listener(tweets_file)
+
+        auth = OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
+        auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
+
+        stream = Stream(auth, listener)
+        stream.filter(track=hash_tags)
+
+def main():
+    hash_tags = ['Donald Trump', 'Joe Biden']
+    tweets_file = 'live_tweets.json'
+
+    tweet_streamer = Streamer()
+    tweet_streamer.stream_tweets(tweets_file, hash_tags)
 
 
 if __name__ == '__main__':
-    load_dotenv(find_dotenv())
-    listener = TweetListener()
-
-    auth = OAuthHandler(os.environ.get("CONSUMER_KEY"),
-                        os.environ.get("CONSUMER_SECRET"))
-
-    auth.set_access_token(os.environ.get("ACCESS_TOKEN"),
-                          os.environ.get("ACCESS_TOKEN_SECRET"))
-
-    stream = Stream(auth, listener)
-
-    stream.filter(track=['Donald Trump', 'Joe Biden'])
+    main()

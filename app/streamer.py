3dbe4da5e93250238ccb8c71980adfccf072883c
Saurabh Mishra <saurab.mish@gmail.com>
2020-11-25 11:03:21 -0800

    stream tweets based on hash tag(s)

--- app/streamer.py
+++ app/streamer.py
@@ -4,13 +4,14 @@ import keys
 from listener import TwitterListener
 from authenticator import TwitterAuthenticator
 
+
 class Streamer():
 
     def __init__(self):
         self.twitter_authenticator = TwitterAuthenticator()
 
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
-        listener = TwitterListener(fetched_tweets_filename)
+    def stream_tweets(self, hash_tag_list):
+        listener = TwitterListener()
         auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
         stream.filter(track=hash_tag_list)
@@ -18,11 +19,10 @@ class Streamer():
 
 def main():
 
-    hash_tags = ['Donald Trump', 'Joe Biden']
-    tweets_file = 'live_tweets.json'
-
     tweet_streamer = Streamer()
-    tweet_streamer.stream_tweets(tweets_file, hash_tags)
+    hash_tag = ['Donald Trump']
+    # hash_tags = ['Donald Trump', 'Joe Biden']     # returns sentiment for both trackers
+    tweet_streamer.stream_tweets(hash_tag)
 
 
 if __name__ == '__main__':

dfb00b72b21d427818e83562fb2240ae8f35852f
Saurabh Mishra <saurab.mish@gmail.com>
2020-11-28 16:09:51 -0800

    updated environment variable name

--- app/authenticator.py
+++ app/authenticator.py
@@ -5,6 +5,6 @@ from app import keys
 class TwitterAuthenticator:
 
     def authenticate_twitter_app(self):
-        auth = OAuthHandler(keys.API_KEY, keys.API_SECRET_KEY)
+        auth = OAuthHandler(keys.API_KEY, keys.API_KEY_SECRET)
         auth.set_access_token(keys.ACCESS_TOKEN, keys.ACCESS_TOKEN_SECRET)
         return auth

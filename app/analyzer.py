ee2ea3e07b1f88a5941bd7a786ced31e123586fb
Saurabh Mishra <saurab.mish@gmail.com>
2020-11-30 22:21:22 -0800

    added return for each function

--- app/analyzer.py
+++ app/analyzer.py
@@ -23,42 +23,56 @@ class Analyzer:
         self.neutral = 0
 
 
-    def get_tweets_and_analyze(self, filtered_tweets, searchTerm, NoOfTerms):
-        self.tweets = filtered_tweets
-        for tweet in self.tweets:
-            self.tweetText.append(self.cleanTweet(tweet.text).encode('utf-8'))
-            analysis = TextBlob(tweet.text)
-            self.polarity += analysis.sentiment.polarity
+    def get_tweets_and_analyze(self, filtered_tweets, searchTerm, num_tweets):
+        try:
+            self.tweets = filtered_tweets
+            for tweet in self.tweets:
+                self.tweetText.append(self.clean_tweet(tweet.text).encode('utf-8'))
+                analysis = TextBlob(tweet.text)
+                self.polarity += analysis.sentiment.polarity
 
-            if (analysis.sentiment.polarity == 0):
-                self.neutral += 1
-            elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
-                self.wpositive += 1
-            elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
-                self.positive += 1
-            elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
-                self.spositive += 1
-            elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
-                self.wnegative += 1
-            elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
-                self.negative += 1
-            elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
-                self.snegative += 1
+                if (analysis.sentiment.polarity == 0):
+                    self.neutral += 1
+                elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
+                    self.wpositive += 1
+                elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
+                    self.positive += 1
+                elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
+                    self.spositive += 1
+                elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
+                    self.wnegative += 1
+                elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
+                    self.negative += 1
+                elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
+                    self.snegative += 1
+
+            return True
+
+        except Exception as ex:
+            print("EXCEPTION: %s", str(ex))
+        return False
 
 
-    def cleanTweet(self, tweet):
+    def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
 
 
-    def compute_average(self, searchTerm, NoOfTerms):
-        self.positive = self.percentage(self.positive, NoOfTerms)
-        self.wpositive = self.percentage(self.wpositive, NoOfTerms)
-        self.spositive = self.percentage(self.spositive, NoOfTerms)
-        self.negative = self.percentage(self.negative, NoOfTerms)
-        self.wnegative = self.percentage(self.wnegative, NoOfTerms)
-        self.snegative = self.percentage(self.snegative, NoOfTerms)
-        self.neutral = self.percentage(self.neutral, NoOfTerms)
-        self.polarity = self.polarity / NoOfTerms
+    def compute_average(self, num_tweets):
+        try:
+            self.positive = self.percentage(self.positive, num_tweets)
+            self.wpositive = self.percentage(self.wpositive, num_tweets)
+            self.spositive = self.percentage(self.spositive, num_tweets)
+            self.negative = self.percentage(self.negative, num_tweets)
+            self.wnegative = self.percentage(self.wnegative, num_tweets)
+            self.snegative = self.percentage(self.snegative, num_tweets)
+            self.neutral = self.percentage(self.neutral, num_tweets)
+            self.polarity = self.polarity / num_tweets
+
+            return True
+
+        except Exception as ex:
+            print("EXCEPTION: %s", str(ex))
+        return False
 
 
     def percentage(self, part, whole):
@@ -66,93 +80,92 @@ class Analyzer:
         return format(temp, '.2f')
 
 
-    def sentiment_report(self, searchTerm, NoOfTerms):
-        print('Analyzing ' + str(NoOfTerms) + ' tweets from people reacting on term: ' + searchTerm)
-        print()
-        print("General Report: ")
+    def sentiment_report(self, searchTerm, num_tweets):
+        try:
+            print('Analyzing ' + str(num_tweets) + ' tweets from people reacting on term: ' + searchTerm)
+            print()
+            print("General Report: ")
 
-        if (self.polarity == 0):
-            print("Neutral")
-        elif (self.polarity > 0 and self.polarity <= 0.3):
-            print("Weakly positive")
-        elif (self.polarity > 0.3 and self.polarity <= 0.6):
-            print("Positive")
-        elif (self.polarity > 0.6 and self.polarity <= 1):
-            print("Strongly positive")
-        elif (self.polarity > -0.3 and self.polarity <= 0):
-            print("Weakly Negative")
-        elif (self.polarity > -0.6 and self.polarity <= -0.3):
-            print("Negative")
-        elif (self.polarity > -1 and self.polarity <= -0.6):
-            print("Strongly Negative")
+            if (self.polarity == 0):
+                print("Neutral")
+            elif (self.polarity > 0 and self.polarity <= 0.3):
+                print("Weakly positive")
+            elif (self.polarity > 0.3 and self.polarity <= 0.6):
+                print("Positive")
+            elif (self.polarity > 0.6 and self.polarity <= 1):
+                print("Strongly positive")
+            elif (self.polarity > -0.3 and self.polarity <= 0):
+                print("Weakly Negative")
+            elif (self.polarity > -0.6 and self.polarity <= -0.3):
+                print("Negative")
+            elif (self.polarity > -1 and self.polarity <= -0.6):
+                print("Strongly Negative")
 
-        print()
-        print("Detailed Report: ")
-        print(str(self.positive) + "% people thought it was positive")
-        print(str(self.wpositive) + "% people thought it was weakly positive")
-        print(str(self.spositive) + "% people thought it was strongly positive")
-        print(str(self.negative) + "% people thought it was negative")
-        print(str(self.wnegative) + "% people thought it was weakly negative")
-        print(str(self.snegative) + "% people thought it was strongly negative")
-        print(str(self.neutral) + "% people thought it was neutral")
+            print()
+            print("Detailed Report: ")
+            print(str(self.positive) + "% people thought it was positive")
+            print(str(self.wpositive) + "% people thought it was weakly positive")
+            print(str(self.spositive) + "% people thought it was strongly positive")
+            print(str(self.neutral) + "% people thought it was neutral")
+            print(str(self.negative) + "% people thought it was negative")
+            print(str(self.wnegative) + "% people thought it was weakly negative")
+            print(str(self.snegative) + "% people thought it was strongly negative")
 
-        self.plotPieChart(self.positive, self.wpositive, self.spositive,
-            self.negative, self.wnegative, self.snegative, self.neutral,
-            searchTerm, NoOfTerms)
+            return True
+
+        except Exception as ex:
+            print("EXCEPTION: %s", str(ex))
+        return False
 
 
-    def plotPieChart(self, positive, wpositive, spositive, negative,
-                wnegative, snegative, neutral, searchTerm, noOfSearchTerms):
+    def plot_piechart(self, searchTerm, noOfSearchTerms):
+        try:
+            labels = ['positive [' + str(self.positive) + '%]', 'Weakly positive [' + str(self.wpositive) + '%]',
+                      'Strongly positive [' + str(self.spositive) + '%]', 'Neutral [' + str(self.neutral) + '%]',
+                      'Negative [' + str(self.negative) + '%]', 'Weakly Negative [' + str(self.wnegative) + '%]',
+                      'Strongly Negative [' + str(self.snegative) + '%]']
 
-        labels = ['positive [' + str(positive) + '%]', 'Weakly positive ['
-                  + str(wpositive) + '%]','Strongly positive [' + str(spositive)
-                  + '%]', 'Neutral [' + str(neutral) + '%]', 'Negative ['
-                  + str(negative) + '%]', 'Weakly Negative [' + str(wnegative)
-                  + '%]', 'Strongly Negative [' + str(snegative) + '%]']
+            sizes = [self.positive, self.wpositive, self.spositive, self.neutral, self.negative, self.wnegative, self.snegative]
+            colors = ['yellowgreen','lightgreen','darkgreen', 'gold', 'red','lightsalmon','darkred']
+            patches, texts = plt.pie(sizes, colors=colors, startangle=90)
+            plt.legend(labels, loc="best")
+            plt.title('Analyzing ' + str(noOfSearchTerms) + ' tweets from people reacting on term: ' + searchTerm)
+            plt.axis('equal')
+            plt.tight_layout()
+            # plt.show()
+            plt.savefig('app/static/images/piechart.png')
 
-        sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
+            return True
 
-        colors = ['yellowgreen','lightgreen','darkgreen', 'gold', 'red','lightsalmon','darkred']
-
-        patches, texts = plt.pie(sizes, colors=colors, startangle=90)
-
-        plt.legend(labels, loc="best")
-
-        plt.title('Analyzing ' + str(noOfSearchTerms) + ' tweets from people reacting on term: ' + searchTerm)
-
-        plt.axis('equal')
-
-        plt.tight_layout()
-
-        # plt.show()
-        plt.savefig('app/static/images/piechart.png')
+        except Exception as ex:
+            print("EXCEPTION: %s", str(ex))
+        return False
 
 
     def create_chart(self, search_term, number_of_searches):
         twitter_client = TwitterClient()
-        twitter_api = twitter_client.get_twitter_client_api()
         filtered_tweets = twitter_client.get_tweets(search_term, number_of_searches)
         self.get_tweets_and_analyze(filtered_tweets, search_term, number_of_searches)
         self.compute_average(search_term, number_of_searches)
         self.sentiment_report(search_term, number_of_searches)
 
 
-
+'''
 def main():
 
     tweet_analyzer = Analyzer()
-
     twitter_client = TwitterClient()
-    twitter_api = twitter_client.get_twitter_client_api()
 
     search_term = 'trump'
-    number_of_searches = 20
+    number_of_searches = 10
 
     filtered_tweets = twitter_client.get_tweets(search_term, number_of_searches)
     tweet_analyzer.get_tweets_and_analyze(filtered_tweets, search_term, number_of_searches)
-    tweet_analyzer.compute_average(search_term, number_of_searches)
+    tweet_analyzer.compute_average(number_of_searches)
     tweet_analyzer.sentiment_report(search_term, number_of_searches)
+    tweet_analyzer.plot_piechart(search_term, number_of_searches)
 
 
 if __name__ == '__main__':
     main()
+'''

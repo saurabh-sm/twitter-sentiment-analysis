f81652f4b27d4892d615c52e0007ad57d8b56816
Saurabh Mishra <saurab.mish@gmail.com>
2020-11-30 22:27:31 -0800

    added test cases for analyzer

---
+++ tests/unit/test_analyzer.py
@@ -0,0 +1,36 @@
+import unittest
+
+from app.analyzer import Analyzer
+from app.client   import TwitterClient
+
+class TestAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        self.search_term = 'trump'
+        self.number_of_tweets = 10
+        self.analyzer = Analyzer()
+        self.client   = TwitterClient()
+
+    def test_filtered_tweets(self):
+        filtered_tweets = self.client.get_tweets(self.search_term, self.number_of_tweets)
+        self.assertIsNotNone(filtered_tweets)
+        return filtered_tweets
+
+    def test_clean_tweet(self):
+        self.assertIsNotNone(self.analyzer.clean_tweet("This is a test string"))
+
+    def test_get_tweets_and_analyze(self):
+        filtered_tweets = self.test_filtered_tweets()
+        self.assertTrue(self.analyzer.get_tweets_and_analyze(filtered_tweets, self.search_term, self.number_of_tweets))
+
+    def test_percentage(self):
+        self.assertIsNotNone(self.analyzer.percentage(2, self.number_of_tweets))
+
+    def test_compute_average(self):
+        self.assertTrue(self.analyzer.compute_average(self.number_of_tweets))
+
+    def test_sentiment_report(self):
+        self.assertTrue(self.analyzer.sentiment_report(self.search_term, self.number_of_tweets))
+
+    def test_plot_pie_chart(self):
+        self.assertTrue(self.analyzer.plot_piechart(self.search_term, self.number_of_tweets))

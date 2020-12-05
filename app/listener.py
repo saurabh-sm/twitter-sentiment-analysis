47e167ad2fa8590fa3c373baeacd7030d71a3ebd
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-05 15:58:09 -0800

    exception-free live stream on zsh and flask

--- app/listener.py
+++ app/listener.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from tweepy.streaming import StreamListener
 from textblob import TextBlob
@@ -9,50 +10,72 @@ class TwitterListener(StreamListener):
     def __init__(self):
         self.es = Elasticsearch()
         self.es.indices.create(index='twitter', ignore=400)
+        self.tweets = []
+        self.tweetText = []
+
 
     def on_data(self, data):
         try:
-            dict_data = json.loads(data)
-            tweet = TextBlob(dict_data["text"])
-            print(tweet.sentiment.polarity)
+            raw_tweet = json.loads(data)
+            for tweet in raw_tweet:
+                filtered_tweet = TextBlob(raw_tweet["text"])
+                print("Text extracted from metadata:")
+                print(filtered_tweet, "\n")
+                processed_tweet = self.clean_tweet(str(filtered_tweet))
+                print("Result after cleaning tweet:")
+                print(processed_tweet, "\n")
+                print("Polarity of processed tweet:")
+                print(filtered_tweet.sentiment.polarity, "\n")
 
-            if (tweet.sentiment.polarity == 0):
-                sentiment = "neutral"
-            elif (tweet.sentiment.polarity > 0 and tweet.sentiment.polarity <= 0.3):
-                sentiment = "weak positive"
-            elif (tweet.sentiment.polarity > 0.3 and tweet.sentiment.polarity <= 0.6):
-                sentiment = "positive"
-            elif (tweet.sentiment.polarity > 0.6 and tweet.sentiment.polarity <= 1):
-                sentiment = "strong positive"
-            elif (tweet.sentiment.polarity > -0.3 and tweet.sentiment.polarity <= 0):
-                sentiment = "weak negative"
-            elif (tweet.sentiment.polarity > -0.6 and tweet.sentiment.polarity <= -0.3):
-                sentiment = "negative"
-            else: # (tweet.sentiment.polarity > -1 and tweet.sentiment.polarity <= -0.6):
-                sentiment = "strong negative"
+                if (filtered_tweet.sentiment.polarity == 0):
+                    sentiment = "neutral"
+                elif (filtered_tweet.sentiment.polarity > 0 and filtered_tweet.sentiment.polarity <= 0.3):
+                    sentiment = "weak positive"
+                elif (filtered_tweet.sentiment.polarity > 0.3 and filtered_tweet.sentiment.polarity <= 0.6):
+                    sentiment = "positive"
+                elif (filtered_tweet.sentiment.polarity > 0.6 and filtered_tweet.sentiment.polarity <= 1):
+                    sentiment = "strong positive"
+                elif (filtered_tweet.sentiment.polarity > -0.3 and filtered_tweet.sentiment.polarity <= 0):
+                    sentiment = "weak negative"
+                elif (filtered_tweet.sentiment.polarity > -0.6 and filtered_tweet.sentiment.polarity <= -0.3):
+                    sentiment = "negative"
+                else: # (filtered_tweet.sentiment.polarity > -1 and filtered_tweet.sentiment.polarity <= -0.6):
+                    sentiment = "strong negative"
 
-            print(sentiment)
+                print("General sentiment of processed tweet:")
+                print(sentiment, "\n")
 
-            self.es.index(
-                index = "twitter",
-                doc_type = "test-type",
-                body = {
-                    "author": dict_data["user"]["screen_name"],
-                    "date": dict_data["created_at"],
-                    "message": dict_data["text"],
-                    "polarity": tweet.sentiment.polarity,
-                    "subjectivity": tweet.sentiment.subjectivity,
-                    "sentiment": sentiment
-                })
+                self.es.index(
+                    index = "twitter",
+                    doc_type = "test-type",
+                    body = {
+                        "author": raw_tweet["user"]["screen_name"],
+                        "date": raw_tweet["created_at"],
+                        "message": raw_tweet["text"],
+                        "polarity": filtered_tweet.sentiment.polarity,
+                        "subjectivity": filtered_tweet.sentiment.subjectivity,
+                        "sentiment": sentiment
+                    })
+
+                print('----------------------------------------------------------------------------')
 
             return True
 
-        except Exception as e:
-            print("Data Error: %s", str(e))
-        return False
+        except Exception as ex:
+            print("EXCEPTION:  ", str(ex))
+            return False
+
 
     def on_error(self, status):
         """Repeated access after rate limit will lock developer account."""
         if status == 420:
             return False
         print(status)
+
+
+    def clean_tweet(self, tweet):
+        tweet = re.sub(r'@[A-Za-z0-9]+', '', tweet)     # remove @mentions
+        tweet = re.sub(r'#', '', tweet)                 # remove hashtag indicator
+        tweet = re.sub(r'RT[\s]+', '', tweet)           # remove retweet indicator
+        tweet = re.sub(r'https?:\/\/\S+', '', tweet)    # remove hyperlink
+        return tweet

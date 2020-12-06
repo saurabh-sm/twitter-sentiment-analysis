7a2b1ef4a2d10d1c1a2998e5e89981220d7e5657
Saurabh Mishra <saurab.mish@gmail.com>
2020-12-06 11:07:29 -0800

    complete GET request testing of microservices

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -15,3 +15,13 @@ def test_static_summary_report(client, captured_templates):
     template, context = captured_templates[0]
     assert template.name == "summary-report.html"
 
+def test_static_piechart(client, captured_templates):
+    """GET request."""
+    response = client.get('/static-analysis/piechart')
+    template, context = captured_templates[0]
+    assert template.name == "piechart.html"
+
+def test_dynamic_analysis(client, captured_templates):
+    response = client.get('/dynamic-analysis/')
+    template, context = captured_templates[0]
+    assert template.name == "dynamic-analysis.html"
